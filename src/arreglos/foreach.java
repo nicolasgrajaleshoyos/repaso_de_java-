9d303aa8c52317f6cde5864625e0e5f8f08d965f
nicolasgrajaleshoyos <nicolasgrajaleshoyos@gmail.com>
2024-09-24 20:46:15 -0500

    foreach y arreglos

---
+++ src/arreglos/foreach.java
@@ -0,0 +1,17 @@
+package arreglos;
+
+public class foreach {
+    public static void main(String[] args) {
+
+
+         String [] nombre={"nicolas","mateo","carlos","alejandro","sofia "};
+        //for (int i = 0;i < nombre.length; i++) {
+
+        for (String i:nombre) {
+            System.out.println("los nombres son"+ i);
+        }
+
+
+        }
+    }
+
